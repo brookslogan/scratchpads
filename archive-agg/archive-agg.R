5ae9d6d1dc8dd4db9ad71b3cb380bd6f20ae7dc1
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2025-02-19 13:45:08 -0800

    Focus on packed performance for some epiprocess changes

--- archive-agg/archive-agg.R
+++ archive-agg/archive-agg.R
@@ -2625,11 +2625,11 @@ for (setup_i in seq_len(nrow(setups))) {
   # TODO print...
   withDTthreads(n_threads, {
     print(bench::mark(
-      lgmem_no_del = lapply(seq_len(nrow(test_snapshots)), function(snapshot_i) {
-        subres <- as_tibble(test_snapshots$slide_value[[snapshot_i]]) %>%
-          mutate(version = test_snapshots$version[[snapshot_i]], .after = time_value)
-        subres
-      }) %>% bind_rows() %>% as_epi_archive(compactify = TRUE),
+      ## lgmem_no_del = lapply(seq_len(nrow(test_snapshots)), function(snapshot_i) {
+      ##   subres <- as_tibble(test_snapshots$slide_value[[snapshot_i]]) %>%
+      ##     mutate(version = test_snapshots$version[[snapshot_i]], .after = time_value)
+      ##   subres
+      ## }) %>% bind_rows() %>% as_epi_archive(compactify = TRUE),
       ## nonmod = map_snaps_ea(test_snapshots$slide_value, ~ .x, .compactify_tol = .Machine$double.eps^0.5),
       ## a = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .compactify_tol = .Machine$double.eps^0.5),
       ## a0 = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_a0, .compactify_tol = .Machine$double.eps^0.5),
@@ -2665,13 +2665,13 @@ for (setup_i in seq_len(nrow(setups))) {
       ## l2 = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l2, .compactify_tol = .Machine$double.eps^0.5),
       ## l20 = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l2, .compactify_tol = 0),
       ## l3 = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3, .compactify_tol = .Machine$double.eps^0.5),
-      l30 = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3, .compactify_tol = 0),
+      ## l30 = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3, .compactify_tol = 0),
       ## l30_brl = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3, .compactify_tol = 0, .rbindlist = bind_rows),
       ## l30_vrl = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3, .compactify_tol = 0, .rbindlist = function(DTs) vec_rbind(!!!DTs)),
       ## l30_tblvrl_setDT = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3tbl, .compactify_tol = 0, .rbindlist = function(DTs) vec_rbind(!!!DTs), .converter = setDT),
-      l30_tblvrl_asDT = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3tbl, .compactify_tol = 0, .rbindlist = function(DTs) vec_rbind(!!!DTs), .converter = as.data.table),
-      l30_tblbrl_asDT = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3tbl, .compactify_tol = 0, .rbindlist = bind_rows, .converter = as.data.table),
-      l30_tblrbl = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3tbl, .compactify_tol = 0, .rbindlist = rbindlist, .converter = as.data.table),
+      ## l30_tblvrl_asDT = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3tbl, .compactify_tol = 0, .rbindlist = function(DTs) vec_rbind(!!!DTs), .converter = as.data.table),
+      ## l30_tblbrl_asDT = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3tbl, .compactify_tol = 0, .rbindlist = bind_rows, .converter = as.data.table),
+      ## l30_tblrbl = map_snaps_ea_mod(test_snapshots$slide_value, ~ .x, .epi_diff2 = epi_diff2_l3tbl, .compactify_tol = 0, .rbindlist = rbindlist, .converter = as.data.table),
       packed = map_ea(test_snapshots$slide_value, ~ .x, .compactify_tol = 0),
       ## check = FALSE, # compactify tol vs. not, hopefully
       min_time = 10
