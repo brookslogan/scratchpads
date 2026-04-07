9ec5f7633121e41436646ed3e0f5b56036cdecdf
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2026-04-07 14:43:19 -0700

    Change from tv to tvoffset characterization

--- nowcasting-helpers/playtest-nowcasting-helpers2.R
+++ nowcasting-helpers/playtest-nowcasting-helpers2.R
@@ -42,15 +42,15 @@ predictors <- c("admissions", "google")
 
 stopifnot(target %in% predictors)
 
-predictors_diffkeys <- predictors %>%
+predictors_confkeys <- predictors %>%
   `names<-`({.}) %>%
   lapply(function(predictor) {
-    epix_diffkeys(archive, predictor)
+    epix_confkeys(archive, predictor)
   })
 
-predictors_vtol <- predictors_diffkeys %>%
-  lapply(function(diffkeys) {
-    vtol_preprocess(NULL, archive, diffkeys)
+predictors_vtol <- predictors_confkeys %>%
+  lapply(function(confkeys) {
+    vtol_preprocess(NULL, archive, confkeys)
   })
 
 sum_features_spec <- bind_rows(
@@ -60,46 +60,68 @@ sum_features_spec <- bind_rows(
 
 sum_features_respec <- sum_features_spec %>%
   arrange(abs(add_offset)) %>%
-  mutate(ref_offset = base_offset + add_offset + target_offset,
+  mutate(feature_ref_offset = base_offset + add_offset + target_offset,
          .keep = "unused") %>%
-  mutate(feature = glue::glue_data(., "{predictor}_tlag{ref_offset}_sum{before}_{after}"))
-
-training_tbl <- archive %>%
-  epix_target_evaluation_data(target, -target_offset, anchor_versions = unique(.$DT$version)) %>%
-  na.omit() %>%
-  mutate(reference_time = anchor_version - target_offset)
+  # TODO get slide naming convention, indicate partial
+  mutate(feature = glue::glue_data(., "{predictor}_relt_{feature_ref_offset}_sum_{before}_{after}"))
 
 # for(sum_feature_i in seq_len(nrow(sum_features_respec))) {
 # }
 
 lapply(seq_len(nrow(sum_features_respec)), function(sum_feature_i) {
+  feature <- sum_features_respec$feature[[sum_feature_i]]
   predictor <- sum_features_respec$predictor[[sum_feature_i]]
-  ref_offset <- sum_features_respec$ref_offset[[sum_feature_i]]
+  feature_ref_offset <- sum_features_respec$feature_ref_offset[[sum_feature_i]]
   before <- sum_features_respec$before[[sum_feature_i]]
   after <- sum_features_respec$after[[sum_feature_i]]
   latest %>%
     select(all_of(c(key_colnames(.), predictor))) %>%
     filter(between(.data$time_value,
-                   .env$testing_reference_time + .env$ref_offset - .env$before,
-                   .env$testing_reference_time + .env$ref_offset + .env$after)) %>%
+                   .env$testing_reference_time + .env$feature_ref_offset - .env$before,
+                   .env$testing_reference_time + .env$feature_ref_offset + .env$after)) %>%
     filter(!is.na(.data[[predictor]])) %>%
-    mutate(version = corresponding_diffkey_versions(., archive, predictor)) %>%
-    select(-all_of(predictor)) %>%
-    pack(ek = all_of(key_colnames(latest, exclude = "time_value")),
-         tv = all_of(c("time_value", "version"))) %>%
-    chop("tv") %>%
-    mutate(predictor = .env$predictor) %>%
+    transmute(
+      ek = .[key_colnames(latest, exclude = "time_value")],
+      time_value,
+      conf_version = corresponding_confkey_versions(., archive, predictor)
+    ) %>%
+    mutate(
+      ref_offset = .data$time_value - .env$testing_reference_time,
+      conf_lag = .data$conf_version - time_get_zero_lag_version(.env$testing_reference_time, archive$time_type, vec_ptype(.data$conf_version)),
+      .keep = "unused"
+    ) %>%
+    # pack(tv = all_of(c("time_value", "version"))) %>%
+    # chop("tv") %>%
+    pack(tvoffset = c(ref_offset, conf_lag)) %>%
+    chop("tvoffset") %>%
+    mutate(predictor = .env$predictor, feature = .env$feature) %>%
     {}
 }) %>%
   bind_rows() %>%
-  nest(agg_feature = c(predictor, tv)) %>%
-  # TODO complete eks
+  nest(agg_feature = c(feature, predictor, tvoffset)) %>%
+  # FIXME TODO complete eks
   chop(ek) %>%
   # {
   #   lapply(seq_len(nrow(.)), function(i) {
   #     as.list(.[i,])
   #   })
   # } %>%
+  # transmute(
+  #   ek = map_chr(ek, function(some_ek_set) {
+  #     map(some_ek_set, format) %>%
+  #       reduce(paste, sep = " x ") %>%
+  #       paste(collapse = ", ")
+  #   }),
+  #   agg_feature = map_chr(agg_feature, function(some_agg_feature) {
+  #     tv_chr <- map_chr(some_agg_feature$tv, function(some_tv) {
+  #       paste0("(", some_tv$time_value, ", ", some_tv$version, ")") %>%
+  #         paste(collapse = ", ")
+  #     })
+  #     paste0(some_agg_feature$predictor, " @ ", tv_chr) %>%
+  #       paste(collapse = "; ")
+  #   })
+  # ) %>%
+  # pwalk(function(ek, agg_feature) {cat(ek); cat("\n"); cat(gsub("; ", "\n", agg_feature)); cat("\n---\n")}) %>%
   transmute(
     ek = map_chr(ek, function(some_ek_set) {
       map(some_ek_set, format) %>%
@@ -107,20 +129,32 @@ lapply(seq_len(nrow(sum_features_respec)), function(sum_feature_i) {
         paste(collapse = ", ")
     }),
     agg_feature = map_chr(agg_feature, function(some_agg_feature) {
-      tv_chr <- map_chr(some_agg_feature$tv, function(some_tv) {
-        paste0("(", some_tv$time_value, ", ", some_tv$version, ")") %>%
-          paste(collapse = ", ")
+      tvoffset_chr <- map_chr(some_agg_feature$tvoffset, function(some_tvoffset) {
+        paste0("(", some_tvoffset$ref_offset, ", ", some_tvoffset$conf_lag, ")") %>%
+          paste(collapse = " + ")
       })
-      paste0(some_agg_feature$predictor, " @ ", tv_chr) %>%
+      paste0(some_agg_feature$predictor, " @ ", tvoffset_chr) %>%
         paste(collapse = "; ")
     })
   ) %>%
-  # select(agg_feature, ek) %>%
   pwalk(function(ek, agg_feature) {cat(ek); cat("\n"); cat(gsub("; ", "\n", agg_feature)); cat("\n---\n")}) %>%
   {}
 
-sum_features_respec %>%
-  filter(predictor == "admissions") %>%
-  mutate(testing_start = testing_reference_time + ref_offset - before,
-         testing_end = testing_reference_time + ref_offset + after) %>%
-  nest_join(latest, join_by(between(y$time_value, x$testing_start, x$testing_end)))
+# It may be more efficient to do some of this with joins, though still
+# might need to group by predictor to preserve classes and for confkey
+# stuff.
+
+# sum_features_respec %>%
+#   filter(predictor == "admissions") %>%
+#   mutate(testing_start = testing_reference_time + ref_offset - before,
+#          testing_end = testing_reference_time + ref_offset + after) %>%
+#   nest_join(latest, join_by(between(y$time_value, x$testing_start, x$testing_end)))
+
+
+
+
+
+training_tbl <- archive %>%
+  epix_target_evaluation_data(target, -target_offset, anchor_versions = unique(.$DT$version)) %>%
+  na.omit() %>%
+  mutate(reference_time = anchor_version - target_offset)
