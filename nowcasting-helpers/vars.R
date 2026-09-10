73a45987d02b43d61c93233763dc0ae7d718cc18
Logan C. Brooks <lcbrooks+github@andrew.cmu.edu>
2026-09-10 15:09:40 -0700

    Considering how to incorporate different train vs. test treatment of variables

--- nowcasting-helpers/vars.R
+++ nowcasting-helpers/vars.R
@@ -38,3 +38,32 @@ tvlag_var <- function(source_name, value_name, tlag, vlag) {
     .........
   }
 }
+
+# Should variables have separate functions for training and testing (and evaluation?) fetching?
+# - Separate: one path to allowing differing behavior for target
+#   variable on train vs test set vs evaluation set (e.g., use
+#   semistable if available vs. complain if more stable available
+#   vs. use more stable).
+#   - but this could also be achieved with target being mapped to
+#     different variables in different contexts
+# - Separate: one way to allow simulated/predicted/imputed test-time
+#   data to be subbed into model trained on real observations (e.g.,
+#   in iterative sample models).  Seems would avoid some awkwardness
+#   and near-ambiguity/easily-messed-up-notation in conditional
+#   notation (if want Y ~ linear_features | partition/weighting-stuff,
+#   then Y ~ linear_features | another_feature =
+#   another_feature_imputed might imply something else, and actually Y
+#   ~ linear_features | another_feature ~
+#   another_feature_sample... well no, this has ~ not =, it's fine,
+#   and "proper" way of writing, Y ~ linear_features |
+#   feature = imputed_feature)
+#   - but as weighting stuff would require some sort of function on
+#     RHS like Y ~ X | w(var), seems like might be able to require
+#     function even for discrete stratification? |
+#     stratified(geo_value)? maybe looks bad though
+# - Combined: we don't need differing behavior for predictors, so
+#   predictors could be characterized more cleanly as one function
+# - Combined: it's unclear how many contexts there are... train and
+#   test, maybe, but there's also evaluation, and maybe other
+#   contexts.  More hassle to write and are we really going to make
+#   the exact desired combination of choices desired?
